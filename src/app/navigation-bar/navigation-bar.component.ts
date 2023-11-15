34db13e292dc8d2709d9113b1e394fbb972aecd5
SarJohnson <sej17b@my.fsu.edu>
2023-11-15 17:23:40 -0500

    Added navigation bar

---
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.scss']
+})
+export class NavbarComponent {
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router
+  ) { }
+
+  logoutUser(): void {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    this.router.navigate(['welcome']);
+  }
+}
